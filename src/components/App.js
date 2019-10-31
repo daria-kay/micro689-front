48e9f3fc517704725692d4f6454d9d07a7a18b5f
Daria <daria.kay000@gmail.com>
2019-10-31 22:47:07 +0500

    add top navbar

--- src/components/App.js
+++ src/components/App.js
@@ -3,13 +3,19 @@ import './App.css';
 import {UploadButton} from "./UploadButton/UploadButton"
 import axios from 'axios'
 import {BASE_URL} from '../constants'
+import {Container, Jumbotron, Navbar} from "react-bootstrap";
 
 function App() {
     axios.defaults.baseURL = BASE_URL;
   return (
-      <div className='app'>
-        <UploadButton/>
-      </div>
+      <Container fluid>
+          <Navbar bg='light'>
+              <Navbar.Brand><h3>Черные списки</h3></Navbar.Brand>
+              <Navbar.Collapse className='justify-content-end'>
+                  <UploadButton />
+              </Navbar.Collapse>
+          </Navbar>
+      </Container>
   );
 }
 
--- src/components/UploadButton/UploadButton.js
+++ src/components/UploadButton/UploadButton.js
@@ -3,8 +3,6 @@ import {Component} from 'react'
 import "./style.css"
 import uploadFile from "../../services/FileUploader";
 import {Alert} from "react-bootstrap";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
 
 export class UploadButton extends Component{
 
@@ -12,7 +10,7 @@ export class UploadButton extends Component{
         super(props);
         this.state = {
             isUpload: false,
-            message: "",
+            message: "Просто тестовое сообщение",
             status: 'success'
         };
     }
@@ -21,15 +19,13 @@ export class UploadButton extends Component{
         let msgOpacity = this.state.isUpload ? '100' : '0';
         return (
             <div id='upload-btn-container'>
-                <Row>
-                    <Col lg={2} className='msg'>
+                    <div className='box' id='msg'>
                         <Alert style={{opacity: msgOpacity}}
                                variant={this.state.status}>
                             {this.state.message}
                         </Alert>
-                    </Col>
-                    <Col lg={4}>
-                        <div id='btn'>
+                    </div>
+                        <div className='box' id='btn'>
                             <input type="file"
                                    name="csv"
                                    id="file"
@@ -37,8 +33,6 @@ export class UploadButton extends Component{
                                    onChange={this.uploader}/>
                             <label htmlFor="file">Выбрать файл</label>
                         </div>
-                    </Col>
-                </Row>
             </div>
         );
     }
